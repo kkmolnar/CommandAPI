02bad4c46af9e68ae8812014ca2fa6374e4ad189
Karoly Molnar <km185051@ncr.com>
2021-07-27 14:52:46 +0200

    Added Model and Mock Repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -3,20 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 
 namespace CommandAPI.Controllers
 {
-  /*  [Route("api/commands")]*/
+    /*  [Route("api/commands")]*/
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase
     {
+
+        private readonly ICommandAPIRepo _repository;
+
+        public CommandsController(ICommandAPIRepo repository)
+        {
+            _repository = repository;
+        }
+
+        /*
         [HttpGet]
         public ActionResult<IEnumerable<string>>Get()
         {
             return new string[] {"this", "is","hard", "coded"};
+        }*/
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems =
+            _repository.GetAllCommands();
+            return Ok(commandItems);
         }
 
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem =_repository.GetCommandById(id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
+        }
     }
 }
--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,8 +17,13 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            /*
+        AddTransient: A service is created each time it is requested from  the Service Container.
+        AddScoped: A service is created once per client request(connection).
+        AddSingleton: A service is created once and reused.
+            */
             services.AddControllers();
-
+            services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
