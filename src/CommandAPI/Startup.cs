b3d9f01d3d3cef1c2a6f6810617be8c333868f9a
Karoly Molnar <km185051@ncr.com>
2021-07-30 06:47:28 +0200

    last0630

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -8,15 +8,33 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+            (Configuration.GetConnectionString("PostgreSqlConnection")));
+
+
+
             /*
         AddTransient: A service is created each time it is requested from  the Service Container.
         AddScoped: A service is created once per client request(connection).
